01c3897474422bfcbf78f1baa439270d246cccec
qian <15815242994@163.com>
2020-03-02 15:44:52 +0800

    vue proxy 数据双向绑定

--- src/pages/算法/15-双向数据绑定/Observer.js
+++ src/pages/算法/15-双向数据绑定/Observer.js
@@ -32,39 +32,3 @@ function react(obj, key, val) {
     }
   })
 }
-
-// function observer(obj) {
-//   if (!obj || typeof obj !== 'object') {
-//     return
-//   }
-
-//   Object.keys(obj).forEach(key => {
-//     // Proxy不允许绑定在非对象上
-//     if (obj[key] && typeof obj[key] === 'object') {
-//       obj[key] = defineReactive(obj[key])
-//     }
-//   })
-// }
-
-// function defineReactive(data) {
-//   const dep = new Dep()
-//   return new Proxy(data, {
-//     get(target, key, receiver) {
-//       if (Dep.target) {
-//         dep.depend()
-//       }
-//       if (Reflect.has(target, key)) {
-//         return Reflect.get(target, key, receiver)
-//       }
-//     },
-//     set(target, key, value, receiver) {
-//       // 进行数组操作时，会进行两次set 一次数据改变，一次length改变，两次改变data的值是不变，因此不应该多分发一次消息
-//       dep.notify()
-//       if (!Array.isArray(data) || key !== 'length') {
-//         observer(data)
-//       }
-//       return Reflect.set(target, key, value, receiver)
-//       // console.log('-------通知订阅者--------')
-//     }
-//   })
-// }
