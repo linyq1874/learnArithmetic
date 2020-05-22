f1e46fac4540f690aa6666d6265d02031f283b45
linyiqian <linyiqian@360.cn>
2020-05-22 10:20:28 +0800

    add compose

--- src/pages/算法/26-函数/compose.js
+++ src/pages/算法/26-函数/compose.js
@@ -29,3 +29,41 @@ const superCompose = (...funcs) => {
 
 const g2 = superCompose(addSymbol, toUpper, addSymbol)
 console.log(g2('hello world'))
+
+/**
+ * a(b(...args)) => compose(a, b)
+ * @param {*} params
+ */
+const myCompose = (a, b) => (...args) => a(b(...args))
+
+const mySuperCompose = (...funcs) => {
+  const len = funcs.length
+
+  if (!len) return args => args
+  if (len === 1) return funcs[0]
+
+  return funcs.reduce((prev, cur) => (...args) => prev(cur(...args)))
+}
+
+const g3 = myCompose(addSymbol, toUpper)
+
+console.log('g3', g3('hello world'))
+
+const g4 = mySuperCompose(console.log, addSymbol, toUpper)
+g4('hello world')
+
+const otherCompose = (...func) => (...args) => {
+  let i = func.length - 1
+  if (i < 0) return args
+  let result = func[i].apply(null, args)
+
+  while (--i >= 0) {
+    result = func[i](result)
+  }
+
+  return result
+}
+
+const g5 = otherCompose(console.log, addSymbol, addSymbol, toUpper)
+
+g5('hello world')
